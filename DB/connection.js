7371f66d4d1ca4c81f800094cc32c423de6fd610
Hobz118 <ahmeddehab02@gmail.com>
2025-01-20 19:54:09 +0200

    done

--- DB/connection.js
+++ DB/connection.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   return await mongoose
-    .connect(process.env.DB_LOCAL)
-    .then((res) => console.log(` DB Connected ✅ .....`))
-    .catch((err) => console.log(` Fail to connect DB❌......${err} `));
+    .connect(process.env.DB_Online)
+    .then((res) => console.log(`DB Connected ✅ .....`))
+    .catch((err) => console.log(`Fail to connect DB❌......${err} `));
 };
 
 export default connectDB;
